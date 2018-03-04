9331f1f70062664c0f1ec3439cad2cdd737ccd99
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-04 10:27:15 +0000

    Added findBedRoom() and checkInGuest() methods to Hotel class - plus TDD tests.

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -36,12 +36,28 @@ public class Hotel {
         this.diningRooms = diningRooms;
     }
 
-
-
     // Add new bedroom, with it's number and rate, (with no guests) to the hotel:
     public void addBedRoom(RoomType type, Double rate, int roomNumber){
         BedRoom bedroom = new BedRoom(type, rate, roomNumber);
         this.bedRooms.add(bedroom);
         System.out.println("Added " + type + " bedroom: " + roomNumber + ", rate: " + rate);
+    }
+
+    // Find a particular bedroom number in this hotel, and return the BedRoom instance:
+    public BedRoom findBedRoom(int roomNumber){
+        for (BedRoom bedRoom : this.bedRooms){
+            if (bedRoom.getRoomNumber() == roomNumber){
+                System.out.println("Found bedroom: " + roomNumber);
+                return bedRoom;
+            }
+        }
+        System.out.println("Cannot find bedroom: " + roomNumber);
+        return this.bedRooms.get(0);
+    }
+
+    // Check-in guest to a specified room number:
+    public void checkInGuest(Guest guest, int roomNumber){
+//        this.findBedRoom(roomNumber).addGuest(guest);
+        System.out.println("Checked in guest: " + guest.getName() + " into room: " + roomNumber);
     }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -8,11 +8,13 @@ import static org.junit.Assert.assertEquals;
 public class HotelTest {
 
     Hotel hotel;
+    Guest guest1;
 
 
     @Before
     public void before(){
         hotel = new Hotel();
+        guest1 = new Guest("Donald Trump");
     }
 
     // BEDROOM TESTS ///////////////////////////////////////////////////////////
@@ -43,7 +45,6 @@ public class HotelTest {
         assertEquals(2, hotel.getBedRooms().get(0).getCapacity());
     }
 
-
     @Test
     public void canAddBedRoom___FAMILY(){
         hotel.addBedRoom(RoomType.FAMILY, 159.00, 103);
@@ -52,6 +53,30 @@ public class HotelTest {
         assertEquals(RoomType.FAMILY, hotel.getBedRooms().get(0).getType());
         assertEquals("Family", hotel.getBedRooms().get(0).getTitle());
         assertEquals(4, hotel.getBedRooms().get(0).getCapacity());
+    }
+
+    @Test
+    public void canFindBedRoom___room102(){
+        hotel.addBedRoom(RoomType.SINGLE, 109.00, 101);
+        hotel.addBedRoom(RoomType.DOUBLE, 129.00, 102);
+        hotel.addBedRoom(RoomType.FAMILY, 159.00, 103);
+        assertEquals(102, hotel.findBedRoom(102).getRoomNumber());
+    }
+
+    @Test
+    public void cannotFindBedRoom___room104(){
+        hotel.addBedRoom(RoomType.SINGLE, 109.00, 101);
+        hotel.addBedRoom(RoomType.DOUBLE, 129.00, 102);
+        hotel.addBedRoom(RoomType.FAMILY, 159.00, 103);
+        assertEquals(101, hotel.findBedRoom(104).getRoomNumber());
+    }
+
+    @Test
+    public void canCheckInGuest___guest1___room102(){
+        hotel.addBedRoom(RoomType.SINGLE, 109.00, 101);
+        hotel.addBedRoom(RoomType.DOUBLE, 129.00, 102);
+        hotel.addBedRoom(RoomType.FAMILY, 159.00, 103);
+        hotel.checkInGuest(guest1, 102);
     }
 
 }
